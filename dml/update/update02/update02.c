c0c0c556c13822ec1ff7e511a5c248b7c62cc41f
joohokim <joohokim@cubrid.com>
2021-09-28 17:38:42 +0900

    update02 fix

--- dml/update/update02/update02.c
+++ dml/update/update02/update02.c
@@ -41,7 +41,7 @@ main (int argc, char *argv[])
     }
 
 
-  if (cubrid_log_set_all_in_cond (1) != CUBRID_LOG_SUCCESS)
+  if (cubrid_log_set_all_in_cond (0) != CUBRID_LOG_SUCCESS)
     {
       printf ("[ERROR] %s:%d\n", __FILE__, __LINE__);
       exit (-1);
@@ -102,7 +102,7 @@ main (int argc, char *argv[])
                     printf ("\tdml_type          : %d\n", data_item->dml.dml_type);
                     printf ("\tclassoid          : %lld\n", data_item->dml.classoid);
                     printf ("\tnum_cond_column: %d\n", data_item->dml.num_cond_column);
-                    pritnf ("\tcond_column_index : %d\n", data_item->dml.cond_column_index[0]);
+                    printf ("\tcond_column_index : %d\n", data_item->dml.cond_column_index[0]);
                     printf ("DML SUCCESS \n");
                     printf ("\n");
                   }
