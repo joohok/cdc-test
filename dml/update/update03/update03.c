9d66c6e364c964250af7471798588c62795e8b84
joohokim <joohokim@cubrid.com>
2021-09-28 17:41:43 +0900

    update03 fix

--- dml/update/update03/update03.c
+++ dml/update/update03/update03.c
@@ -102,7 +102,7 @@ main (int argc, char *argv[])
                     printf ("\tdml_type          : %d\n", data_item->dml.dml_type);
                     printf ("\tclassoid          : %lld\n", data_item->dml.classoid);
                     printf ("\tnum_cond_column: %d\n", data_item->dml.num_cond_column);
-                    pritnf ("\tcond_column_index : %d\n", data_item->dml.cond_column_index[0]);
+                    printf ("\tcond_column_index : %d\n", data_item->dml.cond_column_index[0]);
                     printf ("DML SUCCESS \n");
                     printf ("\n");
                   }
